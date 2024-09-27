acd598e9751e4d6f7949f763307c976aff3f144b
Xtreamall <Awesome.kondrashev@outlook.com>
2024-09-27 22:05:04 +0300

    Add files via upload

---
+++ Xtreamall-RBPO/src/main/java/ru/MTUCI/Xtreamall_RBPO/XtreamallRbpoApplication.java
@@ -0,0 +1,13 @@
+package ru.MTUCI.Xtreamall_RBPO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtreamallRbpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtreamallRbpoApplication.class, args);
+	}
+
+}
---
+++ Xtreamall-RBPO/src/test/java/ru/MTUCI/Xtreamall_RBPO/XtreamallRbpoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.MTUCI.Xtreamall_RBPO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtreamallRbpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
